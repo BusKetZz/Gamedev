dec37c543e7a40a56c849d83ed0d95e0a0f8b96b
BusKetZz <busketz2k@gmail.com>
2020-03-31 17:39:30 +0200

    Tutorial/OpenGL_Tutorial/main.cpp: initial implementation to render black window (debug build succes)

--- Tutorial/OpenGL_Tutorial/main.cpp
+++ Tutorial/OpenGL_Tutorial/main.cpp
@@ -1,14 +1,54 @@
 #include "glad.h"
 #include "glfw3.h"
 
+#include <iostream>
+
+
+
+static void FramebufferSize_Callback(GLFWwindow* window, int width,
+                                     int height);
+
 
 
 int main(void)
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	return 0;
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, FramebufferSize_Callback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glViewport(0, 0, 800, 600);
+
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
+
+
+
+static void FramebufferSize_Callback(GLFWwindow* window, int width,
+                                     int height)
+{
+  glViewport(0, 0, width, height);
 }
