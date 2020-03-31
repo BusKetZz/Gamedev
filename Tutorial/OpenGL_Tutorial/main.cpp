525fdea4f61884ea692899ea4ebb380393fc5ac4
BusKetZz <busketz2k@gmail.com>
2020-03-31 16:22:27 +0200

    Tutorial/OpenGL_Tutorial/: main.cpp file initialized

---
+++ Tutorial/OpenGL_Tutorial/main.cpp
@@ -0,0 +1,14 @@
+#include "glad.h"
+#include "glfw3.h"
+
+
+
+int main(void)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	return 0;
+}
